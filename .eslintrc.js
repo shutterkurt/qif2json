694f022efa383b6058d39ef2ee1ee0b5a75d2a36
shutterKurt <shutterkurt@gmail.com>
2020-08-21 16:01:43 -0400

    update linebreak-style based on platform

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,24 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es6: true,
-    node: true,
-    mocha: true,
-  },
-  extends: 'airbnb-base',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-    expect: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {
-    'no-continue': 'off'
-  },
+    env: {
+        commonjs: true,
+        es6: true,
+        node: true,
+        mocha: true,
+    },
+    extends: "airbnb-base",
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
+        expect: "readonly",
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+    },
+    rules: {
+        "no-continue": "off",
+        "linebreak-style": [
+            "error",
+            process.platform === "win32" ? "windows" : "unix",
+        ],
+    },
 };
